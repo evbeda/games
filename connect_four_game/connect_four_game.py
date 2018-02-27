24fbd54b41b0efa339e7366638740d53aad60947
Fernando Mez <fmezzabotta@eventbrite.com>
2018-02-27 13:56:55 -0300

    posicion inicial del tablero

---
+++ connect_four_game/connect_four_game.py
@@ -0,0 +1,15 @@
+
+
+class ConnectFourGame(object):
+
+    def __init__(self):
+        super(ConnectFourGame, self).__init__()
+        self.playing = True
+        self.board_status = [
+                            ['E', 'E', 'E', 'E'],
+                            ['E', 'E', 'E', 'E'],
+                            ['E', 'E', 'E', 'E'],
+                            ['E', 'E', 'E', 'E']
+        ]
+
+
--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,7 @@
 import unittest
 from guess_number_game.test_guess_number_game import TestGuessNumberGame
 from reversi.test_reversi import TestReversi
+from connect_four_game.test_connect_four_game import TestConnectFourGame
 
 
 def suite():
@@ -10,6 +11,7 @@ def suite():
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(TestReversi))
     test_suite.addTest(unittest.makeSuite(TestGuessNumberGame))
+    test_suite.addTest(unittest.makeSuite(TestConnectFourGame))
     return test_suite
 
 
