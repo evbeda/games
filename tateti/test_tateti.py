35970dc12efa0d0e8e4b7e21f8a2cc651aa7d2bd
zahiramanzur <zahiimanzur@gmail.com>
2018-02-27 14:27:58 -0300

    primer test de tablero

--- tateti/test_tateti.py
+++ tateti/test_tateti.py
@@ -4,16 +4,27 @@ from tateti import Tateti
 
 class TestTateti(unittest.TestCase):
 
-    def test_create_tablero(self):
-        tablero = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-        ]
+    def setUp(self):
+        self.tateti = Tateti()
 
-        tateti = Tateti()
-        new_tablero = tateti.create_tablero()
-        self.assertEquals(new_tablero, tablero)
+    def test_piezas_set_0(self):
+        self.tateti.set(0, 1)
+        self.assertEquals(
+            self.tateti.board,
+             "[[0, 'X', 0], [0, 0, 0], [0, 0, 0]]",
+        )
+
+    def test_piezas_set_1(self):
+        self.tateti.set(2, 1)
+        self.assertEquals(
+            self.tateti.board,
+             "[[0, 0, 0], [0, 0, 0], [0, 'X', 0]]",
+        )
+        self.tateti.set(0, 0)
+        self.assertEquals(
+            self.tateti.board,
+             "[['O', 0, 0], [0, 0, 0], [0, 'X', 0]]",
+        )
 
 
 if __name__ == "__main__":
