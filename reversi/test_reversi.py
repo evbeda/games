79319d99263c2dd77994d3885354d3f083e724b3
Cmoyano <cristianmoyano.mza@gmail.com>
2018-02-28 11:32:59 -0300

    Add test wrong movement

--- reversi/test_reversi.py
+++ reversi/test_reversi.py
@@ -9,6 +9,10 @@ class TestReversi(unittest.TestCase):
 
     def test_initial_status(self):
         self.assertTrue(self.game.playing)
+        self.assertEquals(self.game.tablero[3][3], 'B')
+        self.assertEquals(self.game.tablero[3][4], 'W')
+        self.assertEquals(self.game.tablero[4][3], 'W')
+        self.assertEquals(self.game.tablero[4][4], 'B')
 
     def test_initial_next_turn_whites(self):
         self.assertEquals(
@@ -16,5 +20,9 @@ class TestReversi(unittest.TestCase):
             'Turn of the whiteones',
         )
 
-    if __name__ == "__main__":
+    def test_wrong_movement(self):
+        self.assertEquals(self.game.play(1, 1), 'Movimiento no permitido')
+
+
+if __name__ == "__main__":
         unittest.main()
