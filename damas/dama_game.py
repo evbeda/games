fb9c781e18ef5d0420bdeb280dfb0dadee358d66
asaiz <agustinsaiz02@gmail.com>
2018-02-28 11:20:16 -0300

    first moves

--- damas/dama_game.py
+++ damas/dama_game.py
@@ -3,6 +3,7 @@ class DamaGameStart(object):
     def __init__(self):
         super(DamaGameStart, self).__init__()
         self.playing = True
+        self.turn = 'White'
         self.board_status = [
             ['b', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
             [' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'],
@@ -13,6 +14,29 @@ class DamaGameStart(object):
             ['w', ' ', 'w', ' ', 'w', ' ', 'w', ' '],
             [' ', 'w', ' ', 'w', ' ', 'w', ' ', 'w']]
 
-    def playing(self):
-        if self.playing:
-            return 'Your turn'
+    def play(self, x, y, w, z):
+        if(
+            self.turn == 'White' and
+            self.playing and
+            self.board_status[x][y] == 'w'
+        ):
+                self.board_status[x][y] = ' '
+                self.board_status[w][z] = 'w'
+                self.turn = 'Black'
+        elif(
+            self.turn == 'Black' and
+            self.playing and
+            self.board_status[x][y] == 'b'
+        ):
+                self.board_status[x][y] = ' '
+                self.board_status[w][z] = 'b'
+                self.turn = 'White'
+
+    @property
+    def board(self):
+        result = ''
+        for x in xrange(0, 8):
+            for y in xrange(0, 8):
+                result += self.board_status[x][y]
+            result += '\n'
+        return result
--- damas/test.py
+++ damas/test.py
@@ -10,6 +10,22 @@ class TestDamaGame(unittest.TestCase):
     def test_initial_status(self):
         self.assertTrue(self.game.playing)
 
+    def test_initial_turn(self):
+        self.assertEqual('White', self.game.turn)
+
+    def test_initial_board(self):
+        self.assertEqual(
+            self.game.board,
+            'b b b b \n'
+            ' b b b b\n'
+            'b b b b \n'
+            '        \n'
+            '        \n'
+            ' w w w w\n'
+            'w w w w \n'
+            ' w w w w\n'
+        )
+
     def test_initial_board_status(self):
         result = [
             ['b', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
@@ -22,6 +38,54 @@ class TestDamaGame(unittest.TestCase):
             [' ', 'w', ' ', 'w', ' ', 'w', ' ', 'w']
         ]
         self.assertEqual(result, self.game.board_status)
+
+    def test_firts_move(self):
+        result = [
+            ['b', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
+            [' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'],
+            ['b', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', 'w', ' ', 'w', ' ', 'w'],
+            ['w', ' ', 'w', ' ', 'w', ' ', 'w', ' '],
+            [' ', 'w', ' ', 'w', ' ', 'w', ' ', 'w']
+        ]
+        self.game.play(5, 1, 4, 0)
+        self.assertEqual(result, self.game.board_status)
+        self.assertEqual('Black', self.game.turn)
+
+    def test_second_move(self):
+        self.game.play(5, 1, 4, 0)
+        result = [
+            ['b', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
+            [' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'],
+            [' ', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
+            [' ', 'b', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', 'w', ' ', 'w', ' ', 'w'],
+            ['w', ' ', 'w', ' ', 'w', ' ', 'w', ' '],
+            [' ', 'w', ' ', 'w', ' ', 'w', ' ', 'w']
+        ]
+        self.game.play(2, 0, 3, 1)
+        self.assertEqual(result, self.game.board_status)
+        self.assertEqual('White', self.game.turn)
+
+    def test_third_move(self):
+        self.game.play(5, 1, 4, 0)
+        self.game.play(2, 0, 3, 1)
+        result = [
+            ['b', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
+            [' ', 'b', ' ', 'b', ' ', 'b', ' ', 'b'],
+            [' ', ' ', 'b', ' ', 'b', ' ', 'b', ' '],
+            [' ', 'b', ' ', ' ', ' ', ' ', ' ', ' '],
+            ['w', ' ', 'w', ' ', ' ', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', 'w', ' ', 'w'],
+            ['w', ' ', 'w', ' ', 'w', ' ', 'w', ' '],
+            [' ', 'w', ' ', 'w', ' ', 'w', ' ', 'w']
+        ]
+        self.game.play(5, 3, 4, 2)
+        self.assertEqual(result, self.game.board_status)
+        self.assertEqual('Black', self.game.turn)
 
 
 if __name__ == '__main__':
