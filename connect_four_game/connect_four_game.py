bd9a5e71d31b06d391c42b99c7f6e614fb297e72
zahiramanzur <zahiimanzur@gmail.com>
2018-03-01 16:50:27 -0300

    forma del tablero,tamanio,y win

--- connect_four_game/connect_four_game.py
+++ connect_four_game/connect_four_game.py
@@ -6,11 +6,14 @@ class ConnectFourGame(object):
         super(ConnectFourGame, self).__init__()
         self.playing = True
         self.turn = 1
+        self.ficha = ''
         self.board_status = [
-            ['E', 'E', 'E', 'E'],
-            ['E', 'E', 'E', 'E'],
-            ['E', 'E', 'E', 'E'],
-            ['E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E'],
+            ['E', 'E', 'E', 'E', 'E', 'E', 'E'],
         ]
 
     def play(self, column):
@@ -19,15 +22,57 @@ class ConnectFourGame(object):
             self.board_status[0][column] != 'B'
         ):
             if self.turn % 2 != 0:
-                for x in xrange(3, -1, -1):
+                for x in xrange(5, -1, -1):
+                    #import ipdb; ipdb.set_trace()
                     if self.board_status[x][column] == 'E':
                         self.board_status[x][column] = 'W'
+                        self.ficha = 'W'
                         break
             else:
-                for x in xrange(3, -1, -1):
+                for x in xrange(5, -1, -1):
                     if self.board_status[x][column] == 'E':
                         self.board_status[x][column] = 'B'
+                        self.ficha = 'B'
                         break
+            for x in range(5):
+                for y in range(6):
+                    # win oblicuo derecho
+                    if(
+                        x <= 2 and y <= 3 and
+                        self.board_status[x][y] == self.ficha and
+                        self.board_status[x + 1][y + 1] == self.ficha and
+                        self.board_status[x + 2][y + 2] == self.ficha and
+                        self.board_status[x + 3][y + 3] == self.ficha
+                    ):
+                        return 'You win'
+                    # win oblicuo izquierdo
+                    if(
+                        x >= 3 and y >= 3 and
+                        self.board_status[x][y] == self.ficha and
+                        self.board_status[x - 1][y - 1] == self.ficha and
+                        self.board_status[x - 2][y - 2] == self.ficha and
+                        self.board_status[x - 3][y - 3] == self.ficha
+                    ):
+                        return 'You win'
+                    # win horizontal
+                    if(
+                        y <= 3 and
+                        self.board_status[x][y] == self.ficha and
+                        self.board_status[x][y + 1] == self.ficha and
+                        self.board_status[x][y + 2] == self.ficha and
+                        self.board_status[x][y + 3] == self.ficha
+                    ):
+                        return 'You win'
+                    # win vertical
+                    if(
+                        x <= 2 and
+                        self.board_status[x][y] == self.ficha and
+                        self.board_status[x + 1][y] == self.ficha and
+                        self.board_status[x + 2][y] == self.ficha and
+                        self.board_status[x + 3][y] == self.ficha
+                    ):
+                        return 'You win'
+
             self.turn += 1
             return self.board_status
         else:
@@ -38,3 +83,12 @@ class ConnectFourGame(object):
             return 'White plays'
         else:
             return 'Black plays'
+
+    @property
+    def board(self):
+            result = ''
+            for x in xrange(0, 6):
+                for y in xrange(0, 7):
+                    result += self.board_status[x][y]
+                result += '\n'
+            return result
--- connect_four_game/test_connect_four_game.py
+++ connect_four_game/test_connect_four_game.py
@@ -14,43 +14,114 @@ class TestConnectFourGame(unittest.TestCase):
         turn_white = self.game.playingW(1)
         self.assertEqual(turn_white, 'White plays')
 
-    def test_initial_board_status(self):
-        result = [
-                 ['E', 'E', 'E', 'E'],
-                 ['E', 'E', 'E', 'E'],
-                 ['E', 'E', 'E', 'E'],
-                 ['E', 'E', 'E', 'E'],
-        ]
-        self.assertEqual(result, self.game.board_status)
+    def test_initial_Woard_status(self):
+        self.assertEqual(
+            self.game.board,
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+        )
 
     def test_first_move_first_column(self):
-        result = [
-            ['E', 'E', 'E', 'E'],
-            ['E', 'E', 'E', 'E'],
-            ['E', 'E', 'E', 'E'],
-            ['W', 'E', 'E', 'E'],
-        ]
-        self.assertEqual(result, self.game.play(0))
         self.assertTrue(self.game.playing)
+        self.game.play(0)
+        self.assertEqual(
+            self.game.board,
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'WEEEEEE\n',
+        )
 
     def test_second_move_first_column(self):
-        self.game.play(0)
-        result = [
-                 ['E', 'E', 'E', 'E'],
-                 ['E', 'E', 'E', 'E'],
-                 ['B', 'E', 'E', 'E'],
-                 ['W', 'E', 'E', 'E']
-        ]
         self.assertTrue(self.game.playing)
-        self.assertEqual(result, self.game.play(0))
+        self.game.play(0)
+        self.game.play(0)
+        self.assertEqual(
+            self.game.board,
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'BEEEEEE\n'
+            'WEEEEEE\n',
+        )
 
     def test_full_column(self):
         self.game.play(0)
         self.game.play(0)
         self.game.play(0)
         self.game.play(0)
+        self.game.play(0)
+        self.game.play(0)
         self.assertTrue(self.game.playing)
         self.assertEqual('Full column', self.game.play(0))
+
+    def test_win_vertical(self):
+        self.game.play(0)
+        self.game.play(1)
+        self.game.play(0)
+        self.game.play(1)
+        self.game.play(0)
+        self.game.play(1)
+        self.assertEquals('You win', self.game.play(0))
+        self.assertEqual(
+            self.game.board,
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'WEEEEEE\n'
+            'WBEEEEE\n'
+            'WBEEEEE\n'
+            'WBEEEEE\n',
+        )
+
+    def test_win_horizontal(self):
+        self.game.play(0)
+        self.game.play(0)
+        self.game.play(1)
+        self.game.play(1)
+        self.game.play(2)
+        self.game.play(2)
+        self.game.play(3)
+        self.game.play(3)
+        self.assertEquals('You win', self.game.play(4))
+        self.assertEqual(
+            self.game.board,
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'BBBBEEE\n'
+            'WWWWBEE\n',
+        )
+
+    def test_win_diagonalD(self):
+        self.game.play(0)
+        self.game.play(1)
+        self.game.play(1)
+        self.game.play(2)
+        self.game.play(2)
+        self.game.play(3)
+        self.game.play(2)
+        self.game.play(3)
+        self.game.play(3)
+        self.game.play(0)
+        self.game.play(3)
+#        self.assertEquals('You win', self.game.play(3))
+        self.assertEqual(
+            self.game.board,
+            'EEEEEEE\n'
+            'EEEEEEE\n'
+            'EEEWEEE\n'
+            'EEWWEEE\n'
+            'BWWBEEE\n'
+            'WBBBEEE\n',
+        )
 
 
 if __name__ == "__main__":
