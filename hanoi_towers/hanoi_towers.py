98e137771717c461138671f1c99d70331d2d0652
Federico Acosta <facosta@eventbrite.com>
2020-03-30 17:11:15 -0300

    hanoi fixin travis

--- hanoi_towers/hanoi_towers.py
+++ hanoi_towers/hanoi_towers.py
@@ -11,10 +11,12 @@ class HanoiTowers:
         self.tower1 = Tower(cant_tokens)
         self.tower2 = Tower()
         self.tower3 = Tower()
+        self.is_playing = True
 
     def next_turn(self):
 
         if (len(self.tower3.tokens) == self.cant_tokens) or (len(self.tower2.tokens) == self.cant_tokens):
+            self.is_playing = False
             return "You won"
         else:
             return "Plase make your move"
--- hanoi_towers/test_hanoi_towers.py
+++ hanoi_towers/test_hanoi_towers.py
@@ -74,6 +74,7 @@ class TestHanoiTower(unittest.TestCase):
         self.assertEqual(len(hanoi_towers.tower1.tokens), 4)
         self.assertEqual(len(hanoi_towers.tower2.tokens), 0)
         self.assertEqual(len(hanoi_towers.tower3.tokens), 0)
+        self.assertTrue(hanoi_towers.is_playing)
 
     def test_next_turn_win(self):
         token1 = Token(1)
@@ -84,10 +85,12 @@ class TestHanoiTower(unittest.TestCase):
         hanoi_towers.tower3.insert_token(token2)
         hanoi_towers.tower3.insert_token(token1)
         self.assertEqual(hanoi_towers.next_turn(), "You won")
+        self.assertFalse(hanoi_towers.is_playing)
 
     def test_next_turn_still_playing(self):
         hanoi_towers = HanoiTowers(4)
         self.assertEquals(hanoi_towers.next_turn(), "Plase make your move")
+        self.assertTrue(hanoi_towers.is_playing)
 
 
     def test_play_right_move(self):
@@ -123,3 +126,7 @@ class TestHanoiTower(unittest.TestCase):
         hanoi_towers = HanoiTowers(4)
         self.assertEqual(hanoi_towers.board, " 1          \n 2          \n 3          \n 4          \n=== === ===")
 
+    def test_board_display_after_play(self):
+        hanoi_towers = HanoiTowers(4)
+        hanoi_towers.play(hanoi_towers.tower1, hanoi_towers.tower2)
+        self.assertEqual(hanoi_towers.board, " 2          \n 3          \n 4   1      \n=== === ===")
