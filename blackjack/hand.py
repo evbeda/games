96f9e0c3003c029a0dad0fa66e14cca67bd9c2c7
Mario Merida <mamerida2018@gmail.com>
2022-08-16 15:38:47 -0300

    refactor hard blackjack

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -7,6 +7,16 @@ class Hand():
         self.value = 0
         self.counter_as = 0
         self.counter_as_discounted = 0
+        self.dictionary_values = {
+            1 : [0, 11],
+            2 : [12, 22],
+            3 : [13, 23],
+            4 : [14, 24],
+        }
+
+    def subtract_points(self,value_without_as, value_sum_1 ,value_sum_2):
+        if (value_without_as + value_sum_2 == self.value or value_without_as + value_sum_1 == self.value) and self.value > 21:
+            self.value -= 10
 
     def deal_card(self, cards):
         # sumar valor
@@ -23,36 +33,11 @@ class Hand():
                 self.counter_as += 1
             total_value += cardsDictionary[card[0]]
         self.value = total_value
-
         # sumar valor sin ases
         value_without_as = 0
         for card in cards_without_as:
             value_without_as += cardsDictionary[card[0]]
-        if self.counter_as == 1:
-            if value_without_as + 11 == self.value:
-                if self.value > 21:
-                    self.value -= 10
+        if self.counter_as > 0:
+            value_1 , value_2 = self.dictionary_values[self.counter_as]
+            self.subtract_points(value_without_as, value_1, value_2)
 
-        elif self.counter_as == 2:
-            if value_without_as + 22 == self.value:
-                if self.value > 21:
-                    self.value -= 10
-            if value_without_as + 12 == self.value:
-                if self.value > 21:
-                    self.value -= 10
-
-        elif self.counter_as == 3:
-            if value_without_as + 23 == self.value:
-                if self.value > 21:
-                    self.value -= 10
-            if value_without_as + 13 == self.value:
-                if self.value > 21:
-                    self.value -= 10
-
-        elif self.counter_as == 4:
-            if value_without_as + 24 == self.value:
-                if self.value > 21:
-                    self.value -= 10
-            if value_without_as + 14 == self.value:
-                if self.value > 21:
-                    self.value -= 10
